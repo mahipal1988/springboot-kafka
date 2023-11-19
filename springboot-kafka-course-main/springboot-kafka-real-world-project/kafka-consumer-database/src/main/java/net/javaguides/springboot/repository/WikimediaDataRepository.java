a8f12f04810ad8c7f1bab5c61eb025a9bf6f0f62
mahipal1988 <mahipal2021.m@gmail.com>
2023-11-19 18:18:50 -0500

    Sample project changes

--- springboot-kafka-course-main/springboot-kafka-real-world-project/kafka-consumer-database/src/main/java/net/javaguides/springboot/repository/WikimediaDataRepository.java
+++
@@ -1,7 +0,0 @@
-package net.javaguides.springboot.repository;
-
-import net.javaguides.springboot.entity.WikimediaData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
-}
